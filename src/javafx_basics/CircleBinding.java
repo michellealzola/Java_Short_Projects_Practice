0d9cb6898c1f7f39beae4f927aca7c586b29d7d5
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-05 15:15:54 -0600

    Create CircleBinding.java

---
+++ src/javafx_basics/CircleBinding.java
@@ -0,0 +1,37 @@
+package javafx_basics;
+
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class CircleBinding extends Application
+{
+
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+
+	}
+	
+	@Override
+	public void start(Stage primaryStage)
+	{
+		Circle c = new Circle();
+		Group root = new Group(c);
+		
+		Scene scene = new Scene(root, 150, 150);
+		
+		// The following bindings will position and size the circle according to the size of the scene:
+		c.centerXProperty().bind(scene.widthProperty().divide(2));
+		c.centerYProperty().bind(scene.heightProperty().divide(2));
+		c.radiusProperty().bind(Bindings.min(scene.widthProperty(), scene.heightProperty()).divide(2));
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+
+}
