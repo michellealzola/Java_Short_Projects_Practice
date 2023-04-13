5878cca6df77b8c98892d99741d39cf4b30dd519
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-13 10:55:53 -0600

    Bolts and Nuts Inventory Management System

--- src/database_projects/BoltsNutsInventoryManagementSystem.java
+++ src/database_projects/BoltsNutsInventoryManagementSystem.java
@@ -40,11 +40,14 @@ public class BoltsNutsInventoryManagementSystem extends Application
 	private HBox productNameHBox;
 	private HBox productDescriptionHBox;
 	private HBox categoryIDHBox;
+	private VBox addProductVBox;
+	private Button addProductButton;
 	
 	private MenuItem inventoryAdd;
 	private MenuItem categoryAdd;
 	private MenuItem supplierAdd;	
-	private VBox createVBox;
+	
+	
 	
 	// Read menu
 	private Menu readMenu;
@@ -53,12 +56,12 @@ public class BoltsNutsInventoryManagementSystem extends Application
 	private MenuItem categoryMenuItem;
 	private MenuItem supplierMenuItem;	
 	private ListView<String> itemListView;
-	
+	private VBox readVBox;
 	
 	private Menu updateMenu;
 	private Menu deleteMenu;
 	
-	private VBox contentVBox;
+	private BorderPane borderPane;
 	
 	public static void main(String[] args)
 	{
@@ -82,9 +85,14 @@ public class BoltsNutsInventoryManagementSystem extends Application
 		menuBar.getMenus().add(updateMenu);
 		menuBar.getMenus().add(deleteMenu);
 		
-		BorderPane borderPane = new BorderPane();
+		borderPane = new BorderPane();
 		borderPane.setTop(menuBar);
-		borderPane.setCenter(contentVBox);
+
+		readMenu.setOnAction(e2 -> 
+		{
+			borderPane.setCenter(readVBox);
+		});
+		
 		
 		Scene scene = new Scene(borderPane, 1000, 500);
 		scene.getStylesheets().add("nuts_bolts.css");
@@ -97,29 +105,48 @@ public class BoltsNutsInventoryManagementSystem extends Application
 	private void buildCreateMenu(Stage primaryStage)
 	{
 		createMenu = new Menu("Create");
-		productAdd = new MenuItem("Add Product");
-		inventoryAdd = new MenuItem("Add Inventory");
-		categoryAdd = new MenuItem("Add Category");
-		supplierAdd = new MenuItem("Add Supplier");
-		
-		createMenu.getItems().add(productAdd);
-		createMenu.getItems().add(inventoryAdd);
-		createMenu.getItems().add(categoryAdd);
-		createMenu.getItems().add(supplierAdd);
-		
-		productAdd.setOnAction(e -> 
-		{
-			buildAddProductGUI();
-		});
+	    productAdd = new MenuItem("Add Product");
+	    inventoryAdd = new MenuItem("Add Inventory");
+	    categoryAdd = new MenuItem("Add Category");
+	    supplierAdd = new MenuItem("Add Supplier");
+
+	    createMenu.getItems().add(productAdd);
+	    createMenu.getItems().add(inventoryAdd);
+	    createMenu.getItems().add(categoryAdd);
+	    createMenu.getItems().add(supplierAdd);
+
+	    // Add Product
+	    addProductLabel = new Label("Add a Product");
+	    addProductLabel.getStyleClass().add("heading");
+	    
+	    productIDLabel = new Label("Product ID:");
+	    productIDTextField = new TextField();
+	    productIDHBox = new HBox(10, productIDLabel, productIDTextField);
+	    
+	    productNameLabel = new Label("Product Name:");
+	    productNameTextField = new TextField();
+	    productNameHBox = new HBox(10, productNameLabel, productNameTextField);
+	    
+	    productDescriptionLabel = new Label("Product Description:");
+	    productDescriptionTextField = new TextField();
+	    productDescriptionHBox = new HBox(10, productDescriptionLabel, productDescriptionTextField);
+	    
+	    categoryIDLabel = new Label("Category ID:");
+	    categoryIDTextField = new TextField();
+	    categoryIDHBox = new HBox(10, categoryIDLabel, categoryIDTextField);
+	    
+	    addProductButton = new Button("Add Product");
+	    		
+	    addProductVBox = new VBox(10, addProductLabel, productIDHBox, productNameHBox, productDescriptionHBox, categoryIDHBox, addProductButton);
+	    addProductVBox.setPadding(new Insets(25));
+	    
+	    productAdd.setOnAction(e -> {
+	        borderPane.setCenter(addProductVBox);
+	    });
 		
 		
 	}
 
-	private void buildAddProductGUI()
-	{
-		// TODO Auto-generated method stub
-		
-	}
 
 	private void buildReadMenu(Stage primaryStage)
 	{
@@ -200,8 +227,8 @@ public class BoltsNutsInventoryManagementSystem extends Application
 			
 		});		
 
-		contentVBox = new VBox(itemListView);
-		contentVBox.setPadding(new Insets(15));
+		readVBox = new VBox(itemListView);
+		readVBox.setPadding(new Insets(15));
 		
 	}
 
