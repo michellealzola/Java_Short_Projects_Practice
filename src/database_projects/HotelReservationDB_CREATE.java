7d34b9619baf445a369a8eb9c7f36bc4dc8bee44
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-14 08:56:32 -0600

    Hotel Reservation System Desktop App

---
+++ src/database_projects/HotelReservationDB_CREATE.java
@@ -0,0 +1,16 @@
+package database_projects;
+
+import java.sql.*;
+
+public class HotelReservationDB_CREATE
+{
+
+	public static void main(String[] args)
+	{
+		final String DB_URL = "jdbc:derby:HotelReservationDB;create=true";
+		
+		
+
+	}
+
+}
