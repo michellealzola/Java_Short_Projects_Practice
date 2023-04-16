bea7d82597dc2b24ac8aadab20967c6c884f737c
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-15 20:49:23 -0600

    Hotel Reservation System

--- src/database_projects/HotelReservationDB_CREATE.java
+++ src/database_projects/HotelReservationDB_CREATE.java
@@ -9,10 +9,53 @@ public class HotelReservationDB_CREATE
 	{
 		final String DB_URL = "jdbc:derby:HotelReservationDB;create=true";
 		
+		try
+		{
+			Connection conn = DriverManager.getConnection(DB_URL);			
+			
+			dropTables(conn);
+			buildGuestTable(conn);
+			buildRoomTable(conn);
+			buildBookingTable(conn);
+			
+			conn.close();
+			
+			
+			
+			
+		} 
+		catch (SQLException e)
+		{
+			System.out.println("ERROR: " + e.getMessage());
+		}
 		
 		
 		
 
+	}
+
+	private static void dropTables(Connection conn)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void buildGuestTable(Connection conn)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void buildRoomTable(Connection conn)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void buildBookingTable(Connection conn)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
