b0ce40f899a19028cd0fff37ca28f087c2259845
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-16 11:39:04 -0600

    Hotel Reservation System

--- src/database_projects/HotelReservationDB_TEST.java
+++ src/database_projects/HotelReservationDB_TEST.java
@@ -13,20 +13,9 @@ public class HotelReservationDB_TEST
 		{
 			Connection conn = DriverManager.getConnection(DB_URL);
 			
-			Statement stmt = conn.createStatement();
-			
-			String sql = "SELECT * FROM Room";
-			
-			ResultSet result = stmt.executeQuery(sql);
-			
-			while(result.next())
-			{
-				System.out.println(result.getString("id")
-						+ result.getString("room_number")
-						+ result.getString("room_type")
-						+ result.getString("room_price"));
-			}
-			
+			viewGuest(conn);
+			viewRoom(conn);
+			viewBooking(conn);
 			
 			
 			conn.close();
@@ -37,6 +26,51 @@ public class HotelReservationDB_TEST
 			System.out.println("ERROR: " + e.getMessage());
 		}
 
+	}
+
+	private static void viewGuest(Connection conn)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void viewRoom(Connection conn)
+	{
+		Statement stmt;
+		try
+		{
+			stmt = conn.createStatement();
+			
+			String sql = "SELECT * FROM Room";
+			
+			ResultSet result = stmt.executeQuery(sql);
+			
+			System.out.println(result.getMetaData().getColumnName(1)
+					+ result.getMetaData().getColumnName(2)
+					+ result.getMetaData().getColumnName(3)
+					+ result.getMetaData().getColumnName(4));
+			
+			while(result.next())
+			{
+				System.out.println(result.getString("id")
+						+ result.getString("room_number")
+						+ result.getString("room_type")
+						+ result.getString("room_price"));
+			}
+			System.out.println();			
+			
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void viewBooking(Connection conn)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
